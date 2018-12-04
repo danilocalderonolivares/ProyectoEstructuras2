955ca4a6976fc91881d04d76a4339342d4ece8b2
danilocalderonolivares <dcalderono@ucenfotec.ac.cr>
2018-12-03 18:27:24 -0600

    commit para todos

---
+++ ProyectoGoogleMapsConGrafos/Mapa.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace ProyectoGoogleMapsConGrafos
+{
+    partial class Mapa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtLatitud = new System.Windows.Forms.TextBox();
+            this.txtLongitud = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnLlegar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(881, 77);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(267, 32);
+            this.txtDescripcion.TabIndex = 1;
+            // 
+            // txtLatitud
+            // 
+            this.txtLatitud.Location = new System.Drawing.Point(881, 134);
+            this.txtLatitud.Multiline = true;
+            this.txtLatitud.Name = "txtLatitud";
+            this.txtLatitud.Size = new System.Drawing.Size(267, 32);
+            this.txtLatitud.TabIndex = 3;
+            // 
+            // txtLongitud
+            // 
+            this.txtLongitud.Location = new System.Drawing.Point(881, 194);
+            this.txtLongitud.Multiline = true;
+            this.txtLongitud.Name = "txtLongitud";
+            this.txtLongitud.Size = new System.Drawing.Size(267, 32);
+            this.txtLongitud.TabIndex = 4;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(881, 256);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 5;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(962, 256);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnLlegar
+            // 
+            this.btnLlegar.Location = new System.Drawing.Point(1043, 256);
+            this.btnLlegar.Name = "btnLlegar";
+            this.btnLlegar.Size = new System.Drawing.Size(75, 23);
+            this.btnLlegar.TabIndex = 7;
+            this.btnLlegar.Text = "Cómo llegar";
+            this.btnLlegar.UseVisualStyleBackColor = true;
+            this.btnLlegar.Click += new System.EventHandler(this.btnLlegar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(878, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Descripción";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(878, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Latitud";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(878, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Longitud";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(881, 285);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(282, 452);
+            this.dataGridView2.TabIndex = 12;
+            this.dataGridView2.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.SeleccionarRegistro);
+            this.dataGridView2.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.SeleccionarRegistro);
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(12, 12);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(830, 735);
+            this.gMapControl1.TabIndex = 13;
+            this.gMapControl1.Zoom = 0D;
+            this.gMapControl1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.gMapControl1_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(882, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Distancia en metros : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(997, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 15;
+            // 
+            // Mapa
+            // 
+            this.ClientSize = new System.Drawing.Size(1184, 759);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnLlegar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtLongitud);
+            this.Controls.Add(this.txtLatitud);
+            this.Controls.Add(this.txtDescripcion);
+            this.Name = "Mapa";
+            this.Load += new System.EventHandler(this.Mapa_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtLatitud;
+        private System.Windows.Forms.TextBox txtLongitud;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnLlegar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ProyectoGoogleMapsConGrafos/Mapa.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System.Data;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace ProyectoGoogleMapsConGrafos
+{
+    public partial class Mapa : Form
+    {
+        //Variables glovales de marcador
+        GMarkerGoogle vgMarker;
+        GMapOverlay vgMarkerOverlay;
+
+        //Tabla global de almacenamiento de datos 
+        DataTable vgDataTable;
+
+        //Variable glovales de posicionamiento
+        int vgFilaSeleccionada = 0;
+        double vgLatitudInicial = 20.9688132813906;
+        double vgLongitudInicial = -89.6250915527344;
+
+        // Variables globales de enrutamiento
+        bool vgTrazarRuta = false;
+        int vgContadorIndicadoresDeRuta = 0;
+        PointLatLng vgPuntoA;
+        PointLatLng vgPuntoB;
+
+        public Mapa() 
+        {  
+            InitializeComponent();
+        }
+
+        private void Mapa_Load(object sender, EventArgs e)
+        {
+            //Se crean las columnas del DataTable
+            vgDataTable = new DataTable();
+            vgDataTable.Columns.Add(new DataColumn("Descripcion",typeof( string )));
+            vgDataTable.Columns.Add(new DataColumn("Latitud", typeof(double)));
+            vgDataTable.Columns.Add(new DataColumn("Longitud", typeof(double)));
+            //Se agregan registros(Rows) al DataTable
+            //Se agrega el DataTable al DataGridView
+            dataGridView2.DataSource = vgDataTable;
+            //Inicializacion de propiedades por defecto del control gMap
+           
+            
+            GMaps.Instance.Mode = AccessMode.CacheOnly;
+            gMapControl1.DragButton = MouseButtons.Left;
+            gMapControl1.CanDragMap = true;
+            gMapControl1.MapProvider = GoogleMapProvider.Instance;
+            gMapControl1.Position = new PointLatLng(this.vgLatitudInicial, this.vgLongitudInicial);
+            gMapControl1.MinZoom = 0;
+            gMapControl1.MaxZoom = 24;
+            gMapControl1.Zoom = 9;
+            gMapControl1.AutoScroll = true;
+            //Marcador
+            vgMarkerOverlay = new GMapOverlay("Marcadores");
+            GMapOverlay MarkerOverlay = new GMapOverlay("Marcador");
+            vgMarker = new GMarkerGoogle(new PointLatLng(vgLatitudInicial, vgLongitudInicial),GMarkerGoogleType.red);
+            MarkerOverlay.Markers.Add(vgMarker);
+            // Se agrega el overrlay al mapa
+            gMapControl1.Overlays.Add(MarkerOverlay);
+            gMapControl1.Overlays.Add(vgMarkerOverlay);
+        }
+
+        private void SeleccionarRegistro(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+            //Se obtiene el index de la fila que usuario seleciono el dataGridView
+            gMapControl1.Overlays[1].Markers[vgFilaSeleccionada].ToolTipMode = MarkerTooltipMode.OnMouseOver;
+            vgFilaSeleccionada = e.RowIndex;
+            txtDescripcion.Text = dataGridView2.Rows[vgFilaSeleccionada].Cells[0].Value.ToString();
+            txtLatitud.Text = dataGridView2.Rows[vgFilaSeleccionada].Cells[1].Value.ToString();
+            txtLongitud.Text = dataGridView2.Rows[vgFilaSeleccionada].Cells[2].Value.ToString();
+
+            gMapControl1.Overlays[1].Markers[vgFilaSeleccionada].ToolTipMode = MarkerTooltipMode.Always;
+            //Se posiciona mapa en el marcador
+            gMapControl1.Position = gMapControl1.Overlays[1].Markers[vgFilaSeleccionada].Position;
+        }
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //Se inserta un registro en el DataTable
+            vgDataTable.Rows.Add(txtDescripcion.Text,txtLatitud.Text,txtLongitud.Text);
+
+            //Creamos el marcador para moverlo al lugar en el mapa donde le usuario dio doble click.
+            vgMarker = new GMarkerGoogle(new PointLatLng(Convert.ToDouble(txtLatitud.Text), Convert.ToDouble(txtLongitud.Text)), GMarkerGoogleType.green);
+            vgMarker.ToolTipMode = MarkerTooltipMode.OnMouseOver;
+            vgMarker.ToolTipText = String.Format("Ubicacion: \nLatitud: {0} \nLongitud: {1}", txtLatitud.Text, txtLongitud.Text);
+            gMapControl1.Overlays[1].Markers.Add(vgMarker);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //Se elimina un registro del  DataTable
+            vgDataTable.Rows.RemoveAt(vgFilaSeleccionada);
+        }
+
+        private void btnLlegar_Click(object sender, EventArgs e)
+        {
+            this.vgTrazarRuta = true;
+            btnLlegar.Enabled = false;
+        }
+        public void CrearDireccionTrazarRuta(double pLatitud, double pLongitud)
+        {
+            if (this.vgTrazarRuta)
+            {
+                switch (vgContadorIndicadoresDeRuta)
+                {
+                    case 0:
+                        this.vgContadorIndicadoresDeRuta++;
+                        this.vgPuntoA = new PointLatLng(pLatitud,pLongitud);
+                        break;
+                    case 1:
+                        this.vgContadorIndicadoresDeRuta++;
+                        this.vgPuntoB = new PointLatLng(pLatitud, pLongitud);
+                        GMapProviders.GoogleMap.ApiKey = "AIzaSyAKaUlHFCwxvkDF2Itv3twhKgW3-gYmzUA";
+                        GDirections Directions;
+                        //DirectionsStatusCode RutasDireccion = GMapProviders.GoogleMap.GetDirections(out Directions, this.vgPuntoA, this.vgPuntoB, false, false, false, false, false);
+                        GMapRoute RutaObtenida = new GMapRoute(new List<PointLatLng> { vgPuntoA, vgPuntoB }, "Ruta ubicación");
+
+                        RutaObtenida.Stroke.Width = 2;
+                        RutaObtenida.Stroke.Color = Color.SeaGreen;
+
+                        GMapOverlay CapaRutas = new GMapOverlay("Capa Ruta");
+                        CapaRutas.Routes.Add(RutaObtenida);
+                        gMapControl1.Overlays[1] = CapaRutas;
+
+
+                        label2.Text = (RutaObtenida.Distance * 1000).ToString();
+
+                        gMapControl1.Zoom = gMapControl1.Zoom + 1;
+                        gMapControl1.Zoom = gMapControl1.Zoom - 1;
+                        this.vgContadorIndicadoresDeRuta = 0;
+                        this.vgTrazarRuta = false;
+                        btnLlegar.Enabled = true;
+
+                        break;
+                }
+            }
+        }
+
+        private void gMapControl1_OnMarkerDoubleClick(GMapMarker item, MouseEventArgs e)
+        {
+            //Se obtienen la longitud y la latitud  donde el usuario dio doble click
+            double latitud = item.Position.Lat;
+            double longitud = item.Position.Lng;
+            //Se escribe el los txt de latiud y longitus los datos corespondientes.
+            txtLatitud.Text = latitud.ToString();
+            txtLongitud.Text = longitud.ToString();
+            //Creamos el marcador para moverlo al lugar en el mapa donde le usuario dio doble click.
+            //vgMarker.Position = new PointLatLng(latitud, longitud);
+            //vgMarker.ToolTipText = String.Format("Ubicacion: \nLatitud: {0} \nLongitud: {1}", latitud, longitud);
+            this.CrearDireccionTrazarRuta(latitud, longitud);
+        }
+
+        private void gMapControl1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //Se obtienen la longitud y la latitud  donde el usuario dio doble click
+            double latitud = gMapControl1.FromLocalToLatLng(e.X, e.Y).Lat;
+            double longitud = gMapControl1.FromLocalToLatLng(e.X, e.Y).Lng;
+            //Se escribe el los txt de latiud y longitus los datos corespondientes.
+            txtLatitud.Text = latitud.ToString();
+            txtLongitud.Text = longitud.ToString();
+            //Creamos el marcador para moverlo al lugar en el mapa donde le usuario dio doble click.
+            gMapControl1.Overlays[0].Markers[0].Position = new PointLatLng(latitud, longitud);
+            gMapControl1.Overlays[0].Markers[0].ToolTipText = String.Format("Ubicacion: \nLatitud: {0} \nLongitud: {1}", latitud, longitud);
+            this.CrearDireccionTrazarRuta(latitud, longitud);
+
+        }
+    }
+}
